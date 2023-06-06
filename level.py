ed641997e234853025fca6d57acc5cd46977bc5d
para349 <124186113+para349@users.noreply.github.com>
2023-06-05 23:17:17 -0600

    Timestamp at 33:26

--- level.py
+++ level.py
@@ -23,9 +23,12 @@ class Level:
                 x = col_index * TILESIZE
                 y = row_index * TILESIZE
                 if col == 'x':
-                    Tile((x,y),[self.visible_sprites])
+                    Tile((x,y),[self.visible_sprites, self.obstacle_sprites])
+                if col == 'p':
+                    Player((x,y),[self.visible_sprites])
                     
         
     def run(self):
         # update and draw the game
-        pass
+        self.visible_sprites.draw(self.display_surface)
+        self.visible_sprites.update()
