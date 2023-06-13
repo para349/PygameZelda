2b353d006ee2f63ce8f2e2f8d11d410b496f5f73
para349 <124186113+para349@users.noreply.github.com>
2023-06-13 12:06:29 -0600

    Magic chapter in video

--- ui.py
+++ ui.py
@@ -11,6 +11,14 @@ class UI:
         # bar setup
         self.health_bar_rect = pygame.Rect(10,10,HEALTH_BAR_WIDTH,BAR_HEIGHT)
         self.energy_bar_rect = pygame.Rect(10,34,ENERGY_BAR_WIDTH,BAR_HEIGHT)
+        
+        #convert weapon dictionary
+        self.weapon_graphics = []
+        for weapon in weapon_data.values():
+            path = weapon['graphic']
+            weapon = pygame.image.load(path).convert_alpha()
+            self.weapon_graphics.append(weapon)
+        
 
     def show_bar(self,current,max_amount,bg_rect,color):
         # draw bg
@@ -37,10 +45,23 @@ class UI:
         pygame.draw.rect(self.display_surface,UI_BORDER_COLOR,text_rect.inflate(20,20,),3)
         
     def selection_box(self,left,top):
-        bg_rect = pygame.Rect(left,top,ITEM_BOX_SIZE,ITEM_BOX_SIZEwaaa)
-
+        bg_rect = pygame.Rect(left,top,ITEM_BOX_SIZE,ITEM_BOX_SIZE)
+        pygame.draw.rect(self.display_surface,UI_BG_COLOR,bg_rect)
+        pygame.draw.rect(self.display_surface,UI_BORDER_COLOR,bg_rect,3)
+        return bg_rect
+        
+    def weapon_overlay(self,weapon_index):
+        bg_rect = self.selection_box(10,630) # weapon
+        weapon_surf = self.weapon_graphics[weapon_index]
+        weapon_rect = weapon_surf.get_rect(center = bg_rect.center)
+        
+        self.display_surface.blit(weapon_surf,weapon_rect)
+        
     def display(self,player):
         self.show_bar(player.health,player.stats['health'],self.health_bar_rect,HEALTH_COLOR)
         self.show_bar(player.energy,player.stats['energy'],self.energy_bar_rect,ENERGY_COLOR)
         
         self.show_exp(player.exp)
+        
+        self.weapon_overlay(player.weapon_index)
+        self.selection_box(80,635) # magic
